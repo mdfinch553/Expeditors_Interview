2215f0c68771255a60a305bbaf82fd564339ca85
Michael Finch <mikefinch330@gmail.com>
2021-11-09 17:46:47 -0800

    More documentation

--- CodeDriver.java
+++ CodeDriver.java
@@ -14,12 +14,15 @@ public class CodeDriver {
         //hashtable to store unique house addresses
         Hashtable<String, Home> house_addresses = new Hashtable<>();
 
+        //read infor from file and get list of people
         ReadFromFile file_reader = new ReadFromFile();
         people = file_reader.get_list_of_people_from_file();
 
+        //get the table of unique homes, addresses, and numbers of occupants 
         HouseAddressTable households = new HouseAddressTable(people, house_addresses);
         house_addresses = households.get_house_address_table();
 
+        //write information to file
         InfoWriter write_to_file = new InfoWriter(people, house_addresses);
         write_to_file.write_info_to_file();
     }
